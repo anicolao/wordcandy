ba6593acce2c38fdaccc3c91fe5af4fadc2b9e72
anicolao <anicolao@users.noreply.github.com>
2026-02-03 16:45:51 +0000

    deploy: 729616bf6c0c4b237f2c87bf58fcc6a4e8d6deff

---
+++ _app/immutable/chunks/CPuHKxuG.js
@@ -0,0 +1,1 @@
+import{s as e}from"./wtx5Fp39.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
