f32dca27c0782689fc5f72a3cb075401927975ce
anicolao <anicolao@users.noreply.github.com>
2026-02-03 16:36:17 +0000

    deploy: e230dced793c959ef14db877b5d846294c8816c3

---
+++ _app/immutable/chunks/DSsRQ-iN.js
@@ -0,0 +1,1 @@
+import{s as e}from"./CmLz0IaM.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/entry/start.BhnDfXz3.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CmLz0IaM.js";export{o as load_css,r as start};
